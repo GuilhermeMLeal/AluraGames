8c1dfd61d0f59405416b0392597b941fd122c96e
Guilherme <guilherme@educabiz.com>
2026-08-12 21:38:01 -0300

    feat(project): initialized aluGames

---
+++ src/main/kotlin/models/Jogo.kt
@@ -0,0 +1,13 @@
+package com.br.martins.models
+
+class Jogo {
+    var title = ""
+    var category = ""
+    var description = ""
+
+    override fun toString(): String {
+        return "Jogo(title='$title', category='$category', description='$description')"
+    }
+
+
+}
